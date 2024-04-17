9f18478384050f1aabdbfaa9219f5442dc03a4fd
ochi-sho-private-study <ochisyoji1118@gmail.com>
2024-04-17 22:04:00 +0900

    chapter5の学びを追加する

---
+++ scripts/puts_file_descriptor.rb
@@ -0,0 +1,11 @@
+passwd = File.open('/etc/passwd')
+puts passwd.fileno
+
+hosts = File.open('/etc/hosts')
+puts hosts.fileno
+# 開いていた passwd ファイルを閉じる。
+# ファイルディスクリプタ番号は次に開かれるリソースで再利用される
+passwd.close
+
+null = File.open('/dev/null')
+puts null.fileno
